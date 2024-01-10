55100b9806f7b97fcadb2500acafc9c3b5b6b97e
Berlin <tommyonik@gmail.com>
2024-01-09 19:25:51 -0500

    set up server

---
+++ server.js
@@ -0,0 +1,19 @@
+// import modules
+
+import express from 'express'
+
+// create express app
+
+const app = express()
+
+// configure the app
+
+// mount middleware
+
+// mount routes
+
+// tell the app to listen on port 3000
+
+app.listen(3000, function(){
+  console.log('Listening on port 3000');
+})
