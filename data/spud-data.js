be13dcca082982acad45506d31bef65396fd00f8
Berlin <tommyonik@gmail.com>
2024-01-09 19:35:45 -0500

    setup spud dataset

---
+++ data/spud-data.js
@@ -0,0 +1,10 @@
+const spuds = [
+  {text: 'tomato', isSpud: false, _id: 125223},
+  {text: 'potato', isSpud: true, _id: 127904},
+  {text: 'sweet potato', isSpud: true, _id: 139608},
+  {text: 'turnip', isSpud: false, _id: 136548},
+]
+
+export { 
+	spuds
+}
